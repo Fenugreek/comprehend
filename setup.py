6565736f84729ddab53539f005dd09327262c6af
Deepak Subburam <deepak@subburam.org>
2016-06-14 20:58:21 -0400

    standard distutils setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='comprehend',
+      version='0.0.2',
+      description='Deep learning research code built on top of TensorFlow.',
+      author='Deepak Subburam',
+      author_email='deepak@subburam.org',
+      url='https://github.com/Fenugreek/comprehend',
+      packages=['comprehend'],
+      scripts=['test.py'],
+      license="GNU GPL v3")
