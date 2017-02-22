c3b338897bec3b6cb267034ff38745336ba4514f
Deepak Subburam <deepak@subburam.org>
2017-02-22 11:36:53 -0500

    More flexible data loading. Adapt to recent changes in train.py module.

--- run.py
+++ run.py
@@ -2,16 +2,18 @@
 """
 Load/construct various NN models from the commandline, train/run them on data, save to disk.
 """
-import argparse, importlib, cPickle
+import argparse, importlib, cPickle, sys
 
 import numpy as np
 from matplotlib import pyplot
 import bloscpack as bp
 
 import tensorflow as tf
-from comprehend import train, features, functions, layers
+import tensorflow.examples.tutorials.mnist.input_data as mnist_data
+from comprehend import train, features, layers, functions
 
 from tamarind import logging
+import tamarind.functions
 
 # Specify some functions here that are hard to do on the command line.
 # ====================================================================
@@ -34,9 +36,9 @@ target_fn = data_fn
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--model', metavar='<model>', 
-                        help='network architecture to load from networks module. Auto, Denoising, RBM, RNN, or VAE.')
-    parser.add_argument('--module', metavar='<model>', default='comprehend.networks',
-                        help='network architecture to load from networks module. Auto, Denoising, RBM, RNN, or VAE.')
+                        help='network architecture to load from networks module. e.g. Auto, Denoising, RBM, RNN, VAE.')
+    parser.add_argument('--module', metavar='<module>', default='comprehend.networks',
+                        help='find <model> in this module (defaults to module supplied with this library).')
     parser.add_argument('--fromfile', metavar='<filename.dat>',
                         help='previous params.dat file to load from and resume training.')
     parser.add_argument('--add', metavar='<model>',
@@ -55,6 +57,8 @@ if __name__ == '__main__':
                         help='data file with labels for classification training.')
     parser.add_argument('--targets', metavar='<filename.blp>',
                         help='data file with targets for mapping training.')
+    parser.add_argument('--permute', metavar='<filename.blp>',
+                        help='Permute order of rows in input data according to indices in this file.')
     parser.add_argument('--validation', metavar='R', type=float, default=.08,
                         help='fraction of dataset to use as validation set.')
 
@@ -82,31 +86,55 @@ if __name__ == '__main__':
     parser.add_argument('--log', metavar='log_level', default='info',
                         help='debug, info, warning, error or critical.')
     args = parser.parse_args()
-
+    
     logfile = open(args.output+'log.txt', 'w') if args.output else None
     logger = logging.Logger(__file__.split('/')[-1], args.log,
                             logfile=logfile, printer=sys.stdout)
     verbose = logger.level_value < 30 # INFO or more verbose
-
+    
     if not args.model:
         args.model = 'Layers'
         args.module = 'comprehend.layers'
     if '.' not in args.module: args.module = 'comprehend.' + args.module
     module = importlib.import_module(args.module)
-    verbose = not args.quiet
+    coder_class = getattr(module, args.model)
+
 
     # load data
-    dataset = np.hstack(bp.unpack_ndarray_file(fname) for fname in args.data)
+    # ---------
+
+    # Primary dataset
+    if args.data is None:
+        #load mnist as default
+        train_data = mnist_data.read_data_sets('MNIST_data').train
+        dataset = train_data.images
+        if hasattr(coder_class, 'prep_mnist'):
+            dataset = coder_class.prep_mnist(dataset)
+    else:
+        dataset = np.hstack(bp.unpack_ndarray_file(fname) for fname in args.data)
     if data_fn: dataset = data_fn(dataset)
-    if args.labels: labels = bp.unpack_ndarray_file(args.labels)
+    datas = [dataset]
+
+    # auxiliary optional datasets
+    if args.labels:
+        labels = bp.unpack_ndarray_file(args.labels)
+        datas.append(labels)
     if args.targets:
         targets = bp.unpack_ndarray_file(args.targets)
         if target_fn: targets = target_fn(targets)
-    
+        datas.append(targets)
+    if args.permute:
+        perm = bp.unpack_ndarray_file(args.permute)
+        for d in datas: d[:] = d[perm]
+        
     # partition to training and validation
     train_idx = int((1.0 - args.validation) * len(dataset))
     logger.info('Train samples {}, validation samples {}',
                 train_idx, len(dataset) - train_idx)
+
+    
+    # instantiate neural network object
+    # ---------------------------------
 
     kwargs = {}
     for option in args.options or []:
@@ -116,7 +144,6 @@ if __name__ == '__main__':
     if args.bptt: kwargs['seq_length'] = args.bptt
 
     np.random.seed(args.random_seed)
-    coder_class = getattr(module, args.model)
     if args.fromfile:
         coder = coder_class(fromfile=args.fromfile, batch_size=args.batch, verbose=verbose,
                             coding=coding_fn, decoding=decoding_fn, **kwargs)
@@ -137,26 +164,27 @@ if __name__ == '__main__':
     if aux_fn: aux_fn(coder)
 
     if args.labels or args.targets:
-        n_out = np.max(labels) + 1 if args.labels else targets.shape[-1]
+        n_out = targets.shape[-1] if args.targets else np.max(labels) + 1
         if coder.n_hidden != n_out:
             logger.warning("Adding {} output layer to match target", n_out)
             coder = layers.add_class_layer(coder, n_out)
 
+
+    # now execute requested actions
+    # -----------------------------
+    
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
     sess.run(tf.global_variables_initializer())    
     with sess.as_default():
-        kwargs = dict(training_epochs=args.epochs, batch_size=args.batch,
+        kwargs = dict(train_idx=train_idx, training_epochs=args.epochs, batch_size=args.batch,
                       learning_rate=args.learning, logger=logger, costing=cost_fn)
         if args.epochs:
-            if args.labels:
-                train.label_train(sess, coder, dataset[:train_idx], dataset[train_idx:],
-                                  labels[:train_idx], labels[train_idx:], **kwargs)
-            elif args.targets:
-                train.target_train(sess, coder, dataset[:train_idx], dataset[train_idx:],
-                                   targets[:train_idx], targets[train_idx:], **kwargs)
+            if args.targets:
+                train.target_train(sess, coder, datas, **kwargs)
+            elif args.labels:
+                train.label_train(sess, coder, dataset, labels, **kwargs)
             else:
-                train.train(sess, coder, dataset[:train_idx], dataset[train_idx:],
-                            bptt=args.bptt, **kwargs)
+                train.train(sess, coder, dataset, bptt=args.bptt, **kwargs)
         
         if args.output is not None:
             if args.epochs or not args.fromfile:
@@ -166,19 +194,23 @@ if __name__ == '__main__':
                 f.close()
             if args.dump:
                 coder.dump_output(dataset, args.output+args.dump+'.dat',
-                                  kind=args.dump, batch_size=args.batch,)
+                                  kind=args.dump, batch_size=args.batch)
 
         if args.loss:
+            rms_fn = lambda x, y: tf.reduce_mean(tf.squared_difference(x, y),
+                                                 reduction_indices=range(1, coder.input_dims))**.5
             for count, sli in enumerate([slice(train_idx, None),
                                          slice(0, len(dataset)-train_idx)]):
-                if args.labels:
+                if args.targets:
+                    train_args = coder.train_args if hasattr(coder, 'train_args') else coder.init_train_args(train='target')
+                    cost = coder.target_cost(*train_args, function=cost_fn)
+                    cost = train.get_mean_cost(cost, train_args, [d[sli] for d in datas], batch_size=args.batch)
+                    loss = coder.target_cost(*train_args, function=rms_fn)
+                    loss = train.get_mean_cost(loss, train_args, [d[sli] for d in datas], batch_size=args.batch)
+                elif args.labels:
                     cost, loss = \
                           train.get_label_costs(coder, dataset[sli], labels[sli],
                                                 batch_size=args.batch)
-                elif args.targets:
-                    cost, loss = \
-                         train.get_target_costs(coder, dataset[sli], targets[sli],
-                                                batch_size=args.batch, costing=cost_fn)
                 else:
                     cost, loss = \
                           train.get_costs(coder, dataset[sli], bptt=args.bptt,
@@ -191,7 +223,7 @@ if __name__ == '__main__':
         if args.features and hasattr(coder, 'features'):
             results = coder.features(dataset[train_idx:]).squeeze()
             results = results[features.corrsort(results, use_tsp=True)]
-            if results.ndim == 3: results = features.tile(results)
+            if results.ndim == 3: results = features.tile(results, scale=True)
             if args.output is None:
                 pyplot.imshow(results, interpolation='nearest')
                 pyplot.show()
@@ -205,6 +237,7 @@ if __name__ == '__main__':
                     pyplot.show()
                 else:
                     pyplot.imsave(args.output+'stimuli.png', results, origin='upper')
-                
+
     sess.close()
     if logfile is not None: logfile.close()
+
